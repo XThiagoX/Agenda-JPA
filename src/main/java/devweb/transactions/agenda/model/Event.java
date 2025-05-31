d1db22e3df0a51fe40366b8b7e9d9d869428a1d6
Thiago Araujo <thiagoaraujo43034@gmail.com>
2025-05-30 21:03:43 -0300

    feat:Event Construção de evento da agenda

--- src/main/java/devweb/transactions/agenda/model/Event.java
+++ src/main/java/devweb/transactions/agenda/model/Event.java
@@ -2,7 +2,7 @@ package devweb.transactions.agenda.model;
 
 import java.util.Date;
 
-public class EventModel {
+public class Event {
 
     private Long id;
     private String eventName;
@@ -10,12 +10,49 @@ public class EventModel {
     private String startHour;
     private String endHour;
 
-    public EventModel(Long id, String eventName, Date eventDate, String startHour, String endHour) {
+    public Event(Long id, String eventName, Date eventDate, String startHour, String endHour) {
         this.id = id;
         this.eventName = eventName;
         this.eventDate = eventDate;
+        this.startHour = startHour;
+        this.endHour = endHour;
+    }
+    // getters and setters
+
+    public Long getId() {
+        return id;
     }
 
-    // getters and setters
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public Date getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(Date eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public String getStartHour() {
+        return startHour;
+    }
+
+    public void setStartHour(String startHour) {
+        this.startHour = startHour;
+    }
+
+    public String getEndHour() {
+        return endHour;
+    }
+
+    public void setEndHour(String endHour) {
+        this.endHour = endHour;
+    }
 
 }
