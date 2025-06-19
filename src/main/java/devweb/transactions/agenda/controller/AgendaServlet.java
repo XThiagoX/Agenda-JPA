837403594bfd4df80f2ac3930b439dfcc6d19272
Thiago Araujo <thiagoaraujo43034@gmail.com>
2025-06-19 19:35:57 -0300

    Feat: Armazenamento dos eventos adcionados na sessão http

--- src/main/java/devweb/transactions/agenda/controller/AgendaServlet.java
+++ src/main/java/devweb/transactions/agenda/controller/AgendaServlet.java
@@ -3,9 +3,7 @@ package devweb.transactions.agenda.controller;
 import java.io.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.Date;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 import devweb.transactions.agenda.model.Event;
 import jakarta.servlet.ServletException;
@@ -14,7 +12,7 @@ import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "agenda", value = "/agenda")
 public class AgendaServlet extends HttpServlet {
-    Long id = null;
+    String id = UUID.randomUUID().toString();
     String eventName = "";
     Date eventDate = null;
     String startHour = "";
@@ -26,19 +24,33 @@ public class AgendaServlet extends HttpServlet {
         PrintWriter writer = response.getWriter();
         writer.println("<h1>Página está sendo acessada </h1>");
         String name = request.getParameter("name");
-        String date = String.valueOf(LocalDate.parse(request.getParameter("date")));
-        String timeStart =  String.valueOf(LocalTime.parse(request.getParameter("startHour")));
-        String timeEnd = String.valueOf(LocalTime.parse(request.getParameter("endHour")));
+        LocalDate date = LocalDate.parse(request.getParameter("date"));
+
+
+//        String timeStart =  String.valueOf(LocalTime.parse(request.getParameter("startHour")));
+//        String timeEnd = String.valueOf(LocalTime.parse(request.getParameter("endHour")));
 
         // verificando ação de formulário
         String action = request.getParameter("action");{
             if (Objects.equals(action, "add")){
+                Event newEvent = new Event(id, name, date, startHour, endHour);
+
+                HttpSession session = request.getSession();
+                List<Event> events = (List<Event>) session.getAttribute("events");
+
+                if (events == null) {
+                    events = new ArrayList<>();
+                    session.setAttribute("events", events);
+                }
+                events.add(newEvent);
+
+
                 response.setContentType("text/html");
                 writer.println("<ul>");
                 writer.println("<il> Nome: " + name + "</il>");
                 writer.println("<il> Date: " + date + "</il>");
-                writer.println("<il> Horário de Início: " + timeStart + "</il>");
-                writer.println("<il> Horário de Termino: " + timeEnd + "</il>");
+//                writer.println("<il> Horário de Início: " + timeStart + "</il>");
+//                writer.println("<il> Horário de Termino: " + timeEnd + "</il>");
                 writer.println("/<ul>");
 
             }
