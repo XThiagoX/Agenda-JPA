b3d46e9962603f0a2771290ddbf6f5fbcef29fef
Thiago Araujo <thiagoaraujo43034@gmail.com>
2025-06-21 16:03:13 -0300

    Refactor: Remove código desnecessário, código de teste e descomenta váriáveis

--- src/main/java/devweb/transactions/agenda/controller/AgendaServlet.java
+++ src/main/java/devweb/transactions/agenda/controller/AgendaServlet.java
@@ -13,10 +13,6 @@ import jakarta.servlet.annotation.*;
 @WebServlet(name = "agenda", value = "/agenda")
 public class AgendaServlet extends HttpServlet {
     String id = UUID.randomUUID().toString();
-    String eventName = "";
-    Date eventDate = null;
-    String startHour = "";
-    String endHour = "";
 
     private List<Event> getEventList(HttpServletRequest request ) throws ServletException, IOException {
         List<Event> list = (List<Event>) request.getSession().getAttribute("events");
@@ -29,31 +25,23 @@ public class AgendaServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-        PrintWriter writer = response.getWriter();
-        writer.println("<h1>Agenda</h1>");
-
         HttpSession session = request.getSession();
         List<Event> events = getEventList(request);
-
         // verificando ação de formulário
         String action = request.getParameter("action");{
             if (Objects.equals(action, "add")){
-
                 String name = request.getParameter("name");
                 LocalDate date = LocalDate.parse(request.getParameter("date"));
-//        String timeStart =  String.valueOf(LocalTime.parse(request.getParameter("startHour")));
-//        String timeEnd = String.valueOf(LocalTime.parse(request.getParameter("endHour")));
-
+                String startHour =  String.valueOf(LocalTime.parse(request.getParameter("startHour")));
+                String endHour = String.valueOf(LocalTime.parse(request.getParameter("endHour")));
                 if (events == null) {
                     events = new ArrayList<>();
                     session.setAttribute("events", events);
                 }
                 events.add(new Event(id, name, date, startHour, endHour));
-                System.out.println("Action recebida: ");
-                System.out.println("ID para deletar: ");
             } else if (Objects.equals(action, "delete")) {
                 String enventId = request.getParameter("id");
-                events.removeIf(event -> event.getId().equals(enventId));
+                events.removeIf(e -> e.getId().equals(enventId));
             }
             response.sendRedirect(request.getContextPath() + "/agenda");
         }
