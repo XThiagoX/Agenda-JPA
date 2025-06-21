62b9783d11d6d6999d7e745791ce286b7d1433ba
Thiago Araujo <thiagoaraujo43034@gmail.com>
2025-06-21 14:34:32 -0300

    Refactor:(EXtração de método) Encapsulamento da recuperação e criação da lista de eventos

--- src/main/java/devweb/transactions/agenda/controller/AgendaServlet.java
+++ src/main/java/devweb/transactions/agenda/controller/AgendaServlet.java
@@ -18,34 +18,53 @@ public class AgendaServlet extends HttpServlet {
     String startHour = "";
     String endHour = "";
 
+    private List<Event> getEventList(HttpServletRequest request ) throws ServletException, IOException {
+        List<Event> list = (List<Event>) request.getSession().getAttribute("events");
+        if (list == null){
+            list = new ArrayList<>();
+            request.getSession().setAttribute("events", list);
+        }
+        return list;
+    }
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-        HttpSession session = request.getSession();
-        String action = request.getParameter("action");
+        PrintWriter writer = response.getWriter();
+        writer.println("<h1>Agenda</h1>");
 
-        List<Event> events = (List<Event>) session.getAttribute("events");
+        HttpSession session = request.getSession();
+        List<Event> events = getEventList(request);
+
+        String name = request.getParameter("name");
+        LocalDate date = LocalDate.parse(request.getParameter("date"));
+//        String timeStart =  String.valueOf(LocalTime.parse(request.getParameter("startHour")));
+//        String timeEnd = String.valueOf(LocalTime.parse(request.getParameter("endHour")));
+
 
         // verificando ação de formulário
-
+        String action = request.getParameter("action");{
             if (Objects.equals(action, "add")){
-                String name = request.getParameter("name");
-                LocalDate date = LocalDate.parse(request.getParameter("date"));
-                String startHour =  String.valueOf(LocalTime.parse(request.getParameter("startHour")));
-                String endHour = String.valueOf(LocalTime.parse(request.getParameter("endHour")));
-
                 Event newEvent = new Event(id, name, date, startHour, endHour);
+
 
                 if (events == null) {
                     events = new ArrayList<>();
                     session.setAttribute("events", events);
                 }
                 events.add(newEvent);
-            } else if (Objects.equals(action, "delete")) {
-                String id = request.getParameter("id");
-                events.removeIf(event -> event.getId().equals(id));
+                System.out.println("Action recebida: ");
+                System.out.println("ID para deletar: ");
             }
 
-        response.sendRedirect(request.getContextPath() + "/agenda");
+            else if ("delete".equals(action)) {
+                String id = request.getParameter("id");
+                System.out.println("Action recebida: ");
+                System.out.println("ID para deletar: ");
+
+            }
+            response.sendRedirect(request.getContextPath() + "/agenda");
+        }
+
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -53,4 +72,5 @@ public class AgendaServlet extends HttpServlet {
 
         request.getRequestDispatcher("/index.jsp").forward(request, response);
     }
+
 }
